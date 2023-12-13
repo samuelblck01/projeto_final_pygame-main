02e732af0a016aa66895233a6c0654ca34704f96
samuel <samucavini8803@gmail.com>
2023-12-13 14:00:25 -0300

    first commit

---
+++ mecanica.py
@@ -0,0 +1,23 @@
+import random
+from visual import FAIXA_ESQUERDA, FAIXA_DIREITA, JANELA_ALTURA
+
+VEICULO_ALTURA = 250
+
+def alternar_faixa(carro_posicao, faixa):
+  carro_posicao.center = (faixa, carro_posicao.center[1])
+
+# Questão 5, item 1
+def mover_adversario_aleatoriamente(carro_posição, velocidade ):
+  carro_posicao = []
+  carro_posicao.appende[]
+  # Questão 5, itens 3, 4 e 5
+  pass # remova esse comando e escreva seu codigo
+  
+
+# Questão 7, item 1
+def houve_colisao():
+  pass # remova esse comando e escreva seu codigo
+
+# Questão 8, item 1
+def subir_nivel():
+  pass # remova esse comando e escreva seu codigo
